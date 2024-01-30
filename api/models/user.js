d5f21aff796d589132147ebd7324f93ef8a55609
dan gullis <daniel.gullis1@gmail.com>
2024-01-30 12:38:47 +0000

    tests passing

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const root = ''
 
 const UserSchema = new mongoose.Schema({
-  username: {type: String, required: true, unique: true},
+  username: {type: String, unique: true},
   email: { type: String, required: true, unique: true},
   password: { type: String, required: true }, // consider validation options within schema
   bio: String,
