974ae30d351d5d22b16dfdfd302f83066c4e1fbf
Ann Galloway <60460582+AnnGalloway@users.noreply.github.com>
2024-01-30 12:11:39 +0000

    Nav bar looks nice

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css"
+
 const Navbar = () => {
     return (
+        <nav className="navbar">
         <div>
-            <li>
-                <Link to='/'>Homepage</Link>
-            </li>
-            <li>
-                <Link to='/login'>Login</Link>
-            </li>
-            <li>
-                <Link to='/signup'>Sign Up</Link>
-            </li>
-            <li>
-                <Link to='/posts'>Feed</Link>
-            </li>
+            <ul className="nav-links">
+                <li><Link to='/'>Homepage</Link></li>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/signup'>Sign Up</Link></li>
+                <li><Link to='/posts'>Feed</Link></li>
+            </ul>
         </div>
+        </nav>
     );
 }
 
