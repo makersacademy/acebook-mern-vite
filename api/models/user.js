eaeb41044ecc520c8712bef915dade44deb9d9f9
dan gullis <daniel.gullis1@gmail.com>
2024-01-30 11:27:50 +0000

    user schema updated

--- api/models/user.js
+++ api/models/user.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
+const root = ''
+
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  username: {type: String, required: true, unique: true},
+  email: { type: String, required: true, unique: true},
+  password: { type: String, required: true }, // consider validation options within schema
+  bio: String,
+  image: {
+    type: String,
+    get: v => '${root}${v}' // possible solution but requires image hosting server
+  },
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // stores ObjectIds of documents in 'User' collection
+                                                                    // when retrieving a User look to use populate() function to retrieve full documents of friends
 });
 
 const User = mongoose.model("User", UserSchema);
