b18cb211e79f5d912559aadbd807d734d7b5ceb3
Aidan <aidanriley95@gmail.com>
2024-01-30 11:45:36 +0000

    Blank posts can no longer be created

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -2,22 +2,30 @@ const Post = require("../models/post");
 const { generateToken } = require("../lib/token");
 
 const getAllPosts = async (req, res) => {
-  const posts = await Post.find();
-  const token = generateToken(req.user_id);
-  res.status(200).json({ posts: posts, token: token });
+    const posts = await Post.find();
+    const token = generateToken(req.user_id);
+    res.status(200).json({ posts: posts, token: token });
 };
 
 const createPost = async (req, res) => {
-  const post = new Post(req.body);
-  post.save();
+    if (req.body.message !== "") {
+        const post = new Post(req.body);
+        post.save();
 
-  const newToken = generateToken(req.user_id);
-  res.status(201).json({ message: "OK", token: newToken });
+        const newToken = generateToken(req.user_id);
+        res.status(201).json({ message: "OK", token: newToken });
+    } else {
+        const newToken = generateToken(req.user_id);
+        res.status(200).json({
+            message: "posts must not be blank",
+            token: newToken,
+        });
+    }
 };
 
 const PostsController = {
-  getAllPosts: getAllPosts,
-  createPost: createPost,
+    getAllPosts: getAllPosts,
+    createPost: createPost,
 };
 
 module.exports = PostsController;
