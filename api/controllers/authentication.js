d97266a86d42a97c7aedd89446acc427833337c4
mahlvm <mahmoreti@hotmail.com>
2024-01-30 16:08:24 +0000

    add encypt login

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,7 +1,37 @@
 const User = require("../models/user");
 const { generateToken } = require("../lib/token");
+const bcrypt = require('bcrypt'); // includ bcrypt
+
+
+
+////// CODE WITHOUT ENCRYPT ///////
+
+// const createToken = async (req, res) => {
+//   const email = req.body.email;
+//   const password = req.body.password;
+
+//   const user = await User.findOne({ email: email });
+//   if (!user) {
+//     console.log("Auth Error: User not found");
+//     res.status(401).json({ message: "User not found" });
+//   } else if (user.password !== password) {
+//     console.log("Auth Error: Passwords do not match");
+//     res.status(401).json({ message: "Password incorrect" });
+//   } else {
+//     const token = generateToken(user.id);
+//     res.status(201).json({ token: token, message: "OK" });
+//   }
+// };
+
+// const AuthenticationController = {
+//   createToken: createToken,
+// };
+
+
+////// CODE WITH ENCRYPT ///////
 
 const createToken = async (req, res) => {
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -9,10 +39,14 @@ const createToken = async (req, res) => {
   if (!user) {
     console.log("Auth Error: User not found");
     res.status(401).json({ message: "User not found" });
-  } else if (user.password !== password) {
+  } 
+
+  const passwordMatch = await bcrypt.compare(password, user.password); // new
+  if (!passwordMatch) {
     console.log("Auth Error: Passwords do not match");
     res.status(401).json({ message: "Password incorrect" });
-  } else {
+  } 
+  else {
     const token = generateToken(user.id);
     res.status(201).json({ token: token, message: "OK" });
   }
@@ -21,5 +55,7 @@ const createToken = async (req, res) => {
 const AuthenticationController = {
   createToken: createToken,
 };
+
+
 
 module.exports = AuthenticationController;
