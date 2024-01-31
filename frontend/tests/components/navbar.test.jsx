2fdac790052a67dfff2b3f05c7789f8faefc8e21
Holly <holly.page@imshi.co.uk>
2024-01-31 19:53:35 +0000

    logout

--- frontend/tests/components/navbar.test.jsx
+++ frontend/tests/components/navbar.test.jsx
@@ -1,7 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
-import { describe, test, expect } from "vitest";
 
 import { HomePage } from "../../src/pages/Home/HomePage";
 import Navbar from "../../src/components/navbar/Navbar";
@@ -20,8 +19,6 @@ describe("Navbar", () => {
         expect(screen.getByText("Login")).toBeInTheDocument();
         expect(screen.getByText("Sign Up")).toBeInTheDocument();
         expect(screen.getByText("Feed")).toBeInTheDocument();
-        expect(screen.getByText("Logout")).toBeInTheDocument();
-
     });
     test('Navbar link navigates to the Home page', () => {
         render(
@@ -75,40 +72,18 @@ describe("Navbar", () => {
         // Check if the expected navigation has occurred
         expect(window.location.pathname).toBe('/posts');
       });
-
-      test('Navbar logout link deletes token', () => {
+      test('Logout link navigates to the Home page', () => {
         render(
           <BrowserRouter>
             <Navbar />
           </BrowserRouter>
         );
-        // Set a dummy token to local storage so we know something is there
-        window.localStorage.setItem("token", "testToken");
-
-        // Check the token is present
-        expect(window.localStorage.getItem("token")).not.toBeNull();
-
-        // Get the lg out link from the screen
+        // Check if the link is present
         const logoutLink = screen.getByText('Logout');
-
         // Simulate a user click on the link
         fireEvent.click(logoutLink);
-
-        // Make sure token is deleted
-        expect(window.localStorage.getItem("token")).toBeNull(); 
-      });
-
-      test('Logout link redirects to homepage', () => {
-        render(
-          <BrowserRouter>
-            <Navbar />
-          </BrowserRouter>
-        );
-        
-        const logoutLink = screen.getByText('Logout');
-        fireEvent.click(logoutLink);
-        
+        // Check if the expected navigation has occurred
         expect(window.location.pathname).toBe('/');
       });
-});
+})
 
