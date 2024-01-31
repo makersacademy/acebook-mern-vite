294214386c5e4b799bf84ee2f7549e83e360fc1b
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-31 10:49:09 +0000

    added userId + css

--- frontend/src/services/posts.js
+++ frontend/src/services/posts.js
@@ -21,7 +21,7 @@ export const getPosts = async (token) => {
   return data;
 };
 
-export const likePost = async (postId, token) => {
+export const likePost = async (postId, userId, token) => {
   const requestOptions = {
     method: "POST",
     headers: {
@@ -31,7 +31,7 @@ export const likePost = async (postId, token) => {
     body: JSON.stringify({ postId }), // Send the post ID to the backend
   };
 
-  const response = await fetch(`${BACKEND_URL}/posts/${postId}/like`, requestOptions);
+  const response = await fetch(`${BACKEND_URL}/posts/${postId}/like/${userId}`, requestOptions);
 
   if (response.status !== 200) {
     throw new Error("Unable to like the post");
