44f0ea83ff4022684ded25a027e7e9264d17bbb6
Thomas Powell <Thomas.powell838383@gmail.com>
2024-01-31 14:44:29 +0000

    Edited new post file

--- frontend/src/components/Post/NewPost.jsx
+++ frontend/src/components/Post/NewPost.jsx
@@ -1,9 +1,28 @@
-import { useState } from 'react':
+import { useState } from 'react';
 
 const NewPost = ({ onNewPost }) => {
     const [postMessage, setPostMessage] = useState('');
 
     const submit = async (event) => {
+
+        if (postMessage) {
+
+            onNewPost(postMessage)
+
+            setPostMessage('');
+        }
         
-    }
-}
+    };
+
+    return (
+        <form onSubmit={submit}>
+            <textarea
+                value = {postMessage}
+                onChange={(message) => setPostMessage(message.target.value)}
+            ></textarea>
+            <button type='submit'>Post</button>
+        </form>
+    )
+};
+
+export default NewPost
