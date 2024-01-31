d8b442604aa604d7068f4e8fe91e699e968b0987
Thomas Powell <Thomas.powell838383@gmail.com>
2024-01-31 12:32:45 +0000

    newPost started

---
+++ frontend/src/components/Post/NewPost.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react':
+
+const NewPost = ({ onNewPost }) => {
+    const [postMessage, setPostMessage] = useState('');
+
+    const submit = async (event) => {
+        
+    }
+}
