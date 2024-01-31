53c5de2623243fd0935a7f7c2c3507b0886fae34
Nick Torkington <nicktorkington@hotmail.co.uk>
2024-01-31 11:58:24 +0000

    API signup tests completed

--- api/tests/controllers/users.test.js
+++ api/tests/controllers/users.test.js
@@ -12,17 +12,21 @@ describe("/users", () => {
 
     describe("POST, when email and password are provided", () => {
         test("the response code is 201", async () => {
-            const response = await request(app)
-                .post("/users")
-                .send({ email: "poppy@email.com", password: "1234" });
+            const response = await request(app).post("/users").send({
+                username: "poppy",
+                email: "poppy@email.com",
+                password: "Hasd1234@",
+            });
 
             expect(response.statusCode).toBe(201);
         });
 
         test("a user is created", async () => {
-            await request(app)
-                .post("/users")
-                .send({ email: "scarconstt@email.com", password: "1234" });
+            await request(app).post("/users").send({
+                username: "scarc",
+                email: "scarconstt@email.com",
+                password: "Hh@a1234",
+            });
 
             const users = await User.find();
             const newUser = users[users.length - 1];
