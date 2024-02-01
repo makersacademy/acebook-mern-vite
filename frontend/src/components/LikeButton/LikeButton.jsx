55040b577e3caf2567667f832d5ba7439b637501
Ed <edward.gemmill@gmail.com>
2024-02-01 17:23:00 +0000

    close to functional like button

--- frontend/src/components/LikeButton/LikeButton.jsx
+++ frontend/src/components/LikeButton/LikeButton.jsx
@@ -1,8 +1,40 @@
-const LikeButton = (props) => {
-    const { like } = props;
-    const likeUnlike = () => {
-        props.setLike(!like);
-    };
-    return <button onClick={likeUnlike}>{like ? "Unlike" : "Like"}</button>;
+const likeThePost = async (props) => {
+	try {
+		const response = await fetch("http://localhost:3000/posts/likes", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: window.localStorage.getItem("token"),
+			},
+			body: JSON.stringify({ postID: props.postID }),
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+
+		const data = await response.json();
+		console.log(data, "DATATATATATATATATATATA"); // Log the response data
+	} catch (error) {
+		console.error("Error fetching data:", error);
+		// Handle errors appropriately
+	}
 };
+
+const handleClick = async (props) => {
+	await likeThePost(props).then(() => {
+		console.log(props.postID);
+	});
+};
+
+const LikeButton = (props) => {
+	const { like } = props;
+
+	const likeUnlike = () => {
+		props.setLike(!like);
+	};
+
+	return <button onClick={() => handleClick(props)}></button>;
+};
+
 export default LikeButton;
--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -2,19 +2,23 @@ import LikeButton from "../LikeButton/LikeButton";
 import React, { useState } from "react";
 
 const Post = (props) => {
-    const [like, setLike] = useState(false);
+	const [like, setLike] = useState(false);
 
-    const handleLikeUnlike = () => {
-        setLike(!like);
-    };
+	const handleLikeUnlike = () => {
+		setLike(!like);
+	};
 
-    return (
-        <div>
-            <article key={props.post._id}>{props.post.message}</article>
-            <p>{props.post.likes.length}</p>
-            <LikeButton like={like} setLike={handleLikeUnlike} />
-        </div>
-    );
+	return (
+		<div>
+			<article key={props.post._id}>{props.post.message}</article>
+			<p>{props.post.likes.length}</p>
+			<LikeButton
+				postID={props.post._id}
+				like={like}
+				setLike={handleLikeUnlike}
+			/>
+		</div>
+	);
 };
 
 export default Post;
