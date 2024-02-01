db400ad55c68343281634dc7e5465c915eb4a48e
mahlvm <mahmoreti@hotmail.com>
2024-02-01 10:37:57 +0000

    complet taks

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -3,33 +3,6 @@ const { generateToken } = require("../lib/token");
 const bcrypt = require('bcrypt'); // includ bcrypt
 
 
-
-////// CODE WITHOUT ENCRYPT ///////
-
-// const createToken = async (req, res) => {
-//   const email = req.body.email;
-//   const password = req.body.password;
-
-//   const user = await User.findOne({ email: email });
-//   if (!user) {
-//     console.log("Auth Error: User not found");
-//     res.status(401).json({ message: "User not found" });
-//   } else if (user.password !== password) {
-//     console.log("Auth Error: Passwords do not match");
-//     res.status(401).json({ message: "Password incorrect" });
-//   } else {
-//     const token = generateToken(user.id);
-//     res.status(201).json({ token: token, message: "OK" });
-//   }
-// };
-
-// const AuthenticationController = {
-//   createToken: createToken,
-// };
-
-
-////// CODE WITH ENCRYPT ///////
-
 const createToken = async (req, res) => {
 
   const email = req.body.email;
@@ -39,17 +12,19 @@ const createToken = async (req, res) => {
   if (!user) {
     console.log("Auth Error: User not found");
     res.status(401).json({ message: "User not found" });
-  } 
+  } else {
+      const passwordMatch = await bcrypt.compare(password, user.password); // compare password encrypted
+      if (!passwordMatch) {
+        console.log("Auth Error: Passwords do not match");
+        res.status(401).json({ message: "Password incorrect" });
+      } else {
+        const token = generateToken(user.id);
+        res.status(201).json({ token: token, message: "OK" });
+      }
 
-  const passwordMatch = await bcrypt.compare(password, user.password); // new
-  if (!passwordMatch) {
-    console.log("Auth Error: Passwords do not match");
-    res.status(401).json({ message: "Password incorrect" });
-  } 
-  else {
-    const token = generateToken(user.id);
-    res.status(201).json({ token: token, message: "OK" });
   }
+
+
 };
 
 const AuthenticationController = {
