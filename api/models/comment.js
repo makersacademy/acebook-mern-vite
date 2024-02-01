84ce97d8b55d3fda89efa4ed25b0889721b20108
andregeorge <ageorge96.ag@gmail.com>
2024-02-01 10:41:20 +0000

    add comment schema and tests

---
+++ api/models/comment.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+    message: {type: String, require: true},
+    user_id: {type: String, require: true},
+    time_of_comment: {type: String, require: false},
+    // String array of user_id's
+    likes: {type: Array},
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
