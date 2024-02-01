891eb16d81df6ea4bd6ef816e805bc945bd56a18
Ann Galloway <60460582+AnnGalloway@users.noreply.github.com>
2024-02-01 12:17:02 +0000

    date and userID appear for posts

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,26 @@
+  
+  
+  const createDate = (date) => {  // Extract date parts
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // getMonth() is zero-based
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  // Format the date to dd/mm/yyyy hh:mm
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
+};
+
 const Post = (props) => {
-  return <article key={props.post._id}>{props.post.message}</article>;
+
+
+  const datetime = new Date(props.post.reg_time)
+  const date = createDate(datetime)
+  return <article key={props.post._id}>{props.post.message} <br></br>
+    {props.post.userID} <br></br>
+    {date}</article>;
 };
 
 export default Post;
+
+
