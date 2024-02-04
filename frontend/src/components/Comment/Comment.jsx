14215a3a18eae2c7cc9ba1aca5df89afaff6e137
dan gullis <daniel.gullis1@gmail.com>
2024-02-04 21:39:18 +0000

    comment styling

--- frontend/src/components/Comment/Comment.jsx
+++ frontend/src/components/Comment/Comment.jsx
@@ -1,10 +1,12 @@
+import './Comment.css'
+
 export default function Comment({key, _id, message, likes, postedBy, postedAt}) {
     
     const date = new Date(postedAt).toLocaleString('en-UK')
     
     return (
     
-    <div className="comment" key={_id}>
+    <div className="comment-container" key={_id}>
         <p>comment message: {message} </p>
         <p>comment likes: {likes} </p>
         <p>posted by: {postedBy} </p>
