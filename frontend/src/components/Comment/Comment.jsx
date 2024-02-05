31f5ec4cec7d4e906548c0b0c771dcccbe28a666
TomMazzag <itstomazz@gmail.com>
2024-02-05 14:19:26 +0000

    CSS improvements and edit and delete button added in options menu

--- frontend/src/components/Comment/Comment.jsx
+++ frontend/src/components/Comment/Comment.jsx
@@ -1,14 +1,26 @@
 // frontend/src/components/Comment/Comment.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import "./comment.css"
 
 const Comment = ({ comment_data }) => {
+    const [showOptions, setShowOptions] = useState(false)
+    const handleOptions = () => {
+        setShowOptions(!showOptions)
+    }
+
     return (
         <div className="comment">
             <div className='comment-user'>
                 <img src={comment_data.profile_pic} alt="" className='profile-pic'/>
                 <h5 className="comment-author">{comment_data.full_name}</h5>
+                <button className='options' onClick={handleOptions}>...</button>
+                {showOptions && (
+                    <div className='options-menu'>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>
+                )}
             </div>
             <p className="comment-text">{comment_data.message}</p>
         </div>
