8765ac441893c354432bd984500082c696751f1d
mahlvm <mahmoreti@hotmail.com>
2024-02-05 13:31:37 +0000

    HomePage done

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,13 +1,51 @@
+// import { Link } from "react-router-dom";
+
+// import "./HomePage.css";
+
+// export const HomePage = () => {
+//   return (
+//     <div className="home">
+//       <h1>Welcome to Acebook!</h1>
+//       <Link to="/signup">Sign Up</Link>
+//       <Link to="/login">Log In</Link>
+//     </div>
+//   );
+// };
+
+
 import { Link } from "react-router-dom";
 
 import "./HomePage.css";
 
 export const HomePage = () => {
   return (
-    <div className="home">
-      <h1>Welcome to Acebook!</h1>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Log In</Link>
-    </div>
+    <body>
+    
+
+        <div className="content-info">
+          <h1>Welcome to Acebook!</h1>
+          <h3>Acebook helps you connect and share with the people in your life.</h3>
+        </div>
+
+        <div className="content-login">
+            <div className="buttons-box">
+
+              <button className="btn btn-login">
+                <Link to="/login" className="btn">Log In</Link>
+              </button>
+
+
+              <button className="btn btn-signup">
+                <Link to="/signup" className="btn">Create new account</Link>
+              </button>
+
+            </div>
+
+        </div>
+
+
+
+
+    </body>
   );
 };
