b63d018d7e98d4dc6a163479d001142bcd72479a
Gabriela <gabrielaehrenbrink@gmail.com>
2024-02-06 18:20:24 +0000

    when account deleted, likes and comments deleted

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const { generateToken } = require("../lib/token");
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 const mongoose = require("mongoose");
 
 const getUser = async (req, res) => {
@@ -57,32 +58,42 @@ const create = async (req, res) => {
   }
 };
 
-const updateUser = async (id, updatedUserData, options) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(id, updatedUserData, options);
-    return updatedUser;
-  } catch (error) {
-    throw error;
-  }
-};
-
 
 const UsersController = {
   create,
   getUser,
   updateUser: async (req, res) => {
     const { id } = req.params;
-    const { updatedUserData } = req.body;
-
+    const updatedUserData = req.body; // This contains the user data to update.
+    const profilePicPath = req.file ? req.file.path : null; // Get the file path if a file is uploaded.
+    console.log(profilePicPath);
     try {
-      const updatedUser = await updateUser(id, updatedUserData, {new: true});
+      // If there's a new profile picture, add its path to the update data.
+      if (profilePicPath) {
+        
+          
+          let url = `${req.protocol}://${req.get("host")}/${profilePicPath}`;
+        
+        updatedUserData.profile_pic = url;
+      }
+      const existingUserWithEmail = await User.findOne({
+        email: updatedUserData.email,
+        _id: { $ne: id }, // Exclude the current user from the check
+      });
+
+      if (existingUserWithEmail) {
+        return res.status(409).json({ message: "Email already in use" });
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(id, updatedUserData, { new: true });
+      if (!updatedUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
       res.json(updatedUser);
     } catch (error) {
       console.error("Error updating user:", error);
       res.status(500).json({ error: error.message });
-    }
-  },
-
+    }},
 
   deleteUser: async (req, res) => {
     const { id } = req.params;
@@ -91,7 +102,14 @@ const UsersController = {
     try {
       const deletedUser = await User.deleteOne({_id:new mongoose.Types.ObjectId(id)});
       const deletedUserPosts = await Post.deleteMany({user_id:new mongoose.Types.ObjectId(id)});
-      res.json({ message: "User and their posts have been deleted successfully", deletedUserPosts, deletedUser});
+      const deleteUserComments = await Comment.deleteMany({user_id:new mongoose.Types.ObjectId(id)});
+
+          // Remove likes made by the user on posts and comments
+        await Post.updateMany({}, { $pull: { likes: new mongoose.Types.ObjectId(id) } });
+        await Comment.updateMany({}, { $pull: { likes: new mongoose.Types.ObjectId(id) } });
+
+
+      res.json({ message: "User and their posts have been deleted successfully", deletedUserPosts, deleteUserComments, deletedUser});
     } catch (error) {
       console.error("Error deleting user:", error);
       res.status(500).json({ error: error.message });
