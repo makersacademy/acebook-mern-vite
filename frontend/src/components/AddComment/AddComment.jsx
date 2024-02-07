7fb2576b18fec9135c18e3d5c12d98e387a586e1
Ed <edward.gemmill@gmail.com>
2024-02-07 11:06:10 +0000

    comments snag

--- frontend/src/components/AddComment/AddComment.jsx
+++ frontend/src/components/AddComment/AddComment.jsx
@@ -1,59 +1,60 @@
-import { useState } from "react"
-import { postComment } from "../../services/posts"
-import { createNotification } from "../../services/user"
-
-
+import { useState } from "react";
+import { postComment } from "../../services/posts";
+import { createNotification } from "../../services/user";
 
 export default function AddComment({ postId, toggleStateChange, post_userId }) {
-    const [commentText, setCommentText] = useState("")
-    const [token, setToken] = useState(window.localStorage.getItem("token"))
-    const [user, setUserId] = useState(JSON.parse(window.localStorage.getItem("user")))
-    const [errorMessage, setErrorMessage] = useState("")
+	const [commentText, setCommentText] = useState("");
+	const [token, setToken] = useState(window.localStorage.getItem("token"));
+	const [user, setUserId] = useState(
+		JSON.parse(window.localStorage.getItem("user"))
+	);
+	const [errorMessage, setErrorMessage] = useState("");
 
-    const handleChange = (event) => {
-        setCommentText(event.target.value)
-    }
+	const handleChange = (event) => {
+		setCommentText(event.target.value);
+	};
 
-    const submitComment  = async (event) => {
-        // console.log("comment  userId", userId._id)
-        event.preventDefault();
-        if(commentText.length !== 0){
-            try {
-                const result = await postComment(token, commentText, postId, user._id)
-                console.log(result)
-                toggleStateChange()
-                
-                try {
-                    console.log("try", user.username, post_userId, token)
-                    const notificationResult = await createNotification({
-                        username: user.username, 
-                        entity_userId: post_userId,
-                        token: token,
-                        notificationType: "post-comment"
-                    })
-                } catch (error) {
-                    console.log("An error occured while creating a notification")
-                }
+	const submitComment = async (event) => {
+		// console.log("comment  userId", userId._id)
+		event.preventDefault();
+		if (commentText.length !== 0) {
+			try {
+				const result = await postComment(token, commentText, postId, user._id);
+				console.log(result);
+				setCommentText("");
+				toggleStateChange();
 
-            } catch (error) {
-                setErrorMessage("An error occured while posting comment")
-            }
-        } else {
-            setErrorMessage("empty comment") 
-        }
-    }
+				try {
+					console.log("try", user.username, post_userId, token);
+					const notificationResult = await createNotification({
+						username: user.username,
+						entity_userId: post_userId,
+						token: token,
+						notificationType: "post-comment",
+					});
+				} catch (error) {
+					console.log("An error occured while creating a notification");
+				}
+			} catch (error) {
+				setErrorMessage("An error occured while posting comment");
+			}
+		} else {
+			setErrorMessage("empty comment");
+		}
+	};
 
-    return (
-        <>
-        {errorMessage && <p>{errorMessage}</p>}
-        <form onSubmit={submitComment}>
-            <input 
-            type="text" 
-            placeholder="g'day"
-            value={commentText}
-            onChange={handleChange} />
-            <button type="submit">Submit</button>
-        </form>
-        </>
-    )
+	return (
+		<>
+			{errorMessage && <p>{errorMessage}</p>}
+			<form onSubmit={submitComment}>
+				<input
+					type="text"
+					placeholder="g'day"
+					value={commentText}
+					onChange={handleChange}
+				/>
+				<button type="submit">Comment</button>
+			</form>
+		</>
+	);
 }
