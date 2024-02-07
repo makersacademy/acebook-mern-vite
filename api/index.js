aeea110064aecec570ad59552f3ba2d3f72d1524
simonbudden <simon.budden@gmail.com>
2024-02-07 15:35:57 +0000

    Seed data created; runs on server start, wiping db and adding new data. Old photos deleted

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
 // docs: https://github.com/motdotla/dotenv#%EF%B8%8F-usage
 require("dotenv").config();
 
+const seedDatabase = require("./db/seeds.js");
+
+
 const app = require("./app.js");
 const { connectToDatabase } = require("./db/db.js");
 
 const listenForRequests = () => {
 	const port = process.env.PORT || 3000;
 	app.listen(port, () => {
-		console.log("Now listening on port", port);
-	});
-};
-
-connectToDatabase().then(() => {
-	listenForRequests();
+		console.log("Now listening on port", port);	
 });
+}
+
+connectToDatabase()
+	.then(() => seedDatabase())
+	.then(() => {
+		listenForRequests();
+	})
