5d9355820adb69ae3d8b74b238ca02cedefe65fb
Ed <edward.gemmill@gmail.com>
2024-02-07 15:06:01 +0000

    Username appears along with image

--- frontend/src/components/NavBar/UserNavItem.jsx
+++ frontend/src/components/NavBar/UserNavItem.jsx
@@ -1,13 +1,14 @@
-import './navbar.css'
-import { Link } from 'react-router-dom'
+import "./navbar.css";
+
+import { Link } from "react-router-dom";
 
 export default function UserNavItem({ user }) {
-
-    return (
-        <div className="navbar-user-image">
-            <Link to={`/users/${user.username}`}>
-                <img src={user.image}></img>
-            </Link>   
-        </div>
-    )
+	return (
+		<div className="navbar-user-image">
+			<Link to={`/users/${user.username}`}>
+				<img src={user.image} className="navbar-user-image"></img>
+				<p className="navbar-user-username">{user.username}</p>
+			</Link>
+		</div>
+	);
 }
