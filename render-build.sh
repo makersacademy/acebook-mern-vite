a94a406c85e5d88433b3cc70da1b928d0226a6fa
Nick Torkington <nicktorkington@hotmail.co.uk>
2024-02-08 10:54:21 +0000

    1-corrected-render-script

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,9 @@
-ttab -d ./api npm run dev
-ttab -d ./frontend npm run dev
+osascript -e 'tell app "Terminal"
+    do script "cd ./api && npm install && npm run build"
+end tell'
+osascript -e 'tell app "Terminal"
+    do script "cd ./frontend && npm install && npm run build"
+end tell'
+
+# ttab -d ./api npm run build
+# ttab -d ./frontend npm run build
