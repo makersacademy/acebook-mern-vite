210e46da405c2a5f279a69c36124ba2f938a8a26
rosiethesupergeek <rosiethesupergeek.dev@gmail.com>
2024-03-26 16:54:34 +0000

    added navbar component draft

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,11 @@ import React from "react";
 import App from "./App.jsx";
 import "./index.css";
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 // Get the "root" div from index.html.
 // The React application will be inserted into this div.
 const rootElement = document.getElementById("root");
