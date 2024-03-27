cf3db20e4f80f3d1c6e5944e1d5c8f63a2fc5c43
Jason <jason.sayeed@gmail.com>
2024-03-27 15:33:41 +0000

    updated the model user file and TDD

--- api/models/user.js
+++ api/models/user.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
+  fullName: { type: String, required: true },
+  profilePicture: { type: String, required: false },
 });
 
 const User = mongoose.model("User", UserSchema);
