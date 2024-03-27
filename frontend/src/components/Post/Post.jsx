3fd92cf4b407f58328ba3e983da5e3d80b78d972
Jason <jason.sayeed@gmail.com>
2024-03-27 12:33:36 +0000

    completed post.jsx file for component in frontend and TDD

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import Like from "./Like";
+import Comment from "./comment";
+
 const Post = (props) => {
-  return <article key={props.post._id}>{props.post.message}</article>;
+  return (
+    <article className="post" key={props.post._id}>
+      <p className="post-message">{props.post.message}</p>
+      <p className="post-date">{props.post.date}</p>
+      <img className="post-image" src={props.post.image} alt={props.post._id} />
+      <p>
+        <Like />
+      </p>
+      <p>
+        <Comment />
+      </p>
+    </article>
+  );
 };
 
 export default Post;
