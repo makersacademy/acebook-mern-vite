1ea0f57daa6bad34c256c1fb15913a5797e87c41
Maria Jose <mj_hernandezdelprado@hotmail.com>
2024-03-27 12:38:36 +0000

    Update api/models/post with tests and frontend/src/components/post for server display

--- api/models/post.js
+++ api/models/post.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 // A Schema defines the "shape" of entries in a collection. This is similar to
 // defining the columns of an SQL Database.
 const PostSchema = new mongoose.Schema({
-  message: String,
+  username: { type: String, required: true },
+  message: { type: String, required: true },
+  liked: { type: Boolean, default: false },
+  likeCounter: { type: Number, default: 0 },
+  image: {type: String},
+  timeStamps:{type: Date, default:Date.now},
 });
 
 // We use the Schema to create the Post model. Models are classes which we can
@@ -13,6 +18,10 @@ const Post = mongoose.model("Post", PostSchema);
 // These lines will create a test post every time the server starts.
 // You can delete this once you are creating your own posts.
 const dateTimeString = new Date().toLocaleString("en-GB");
-new Post({ message: `Test message, created at ${dateTimeString}` }).save();
+new Post({ username:'Ben',
+          message: 'update model post',
+          liked: true,
+          likeCounter: 1
+          }).save();
 
 module.exports = Post;
--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,11 @@
 const Post = (props) => {
-  return <article key={props.post._id}>{props.post.message}</article>;
+  return (
+    <article key={props.post._id}>
+      <p>Username: {props.post.username}</p>
+      <p>Liked Counter: {props.post.likeCounter}</p>
+      <p>Message: {props.post.message}</p>
+    </article>
+  );
 };
 
 export default Post;
