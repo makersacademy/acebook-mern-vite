b5bdfce4c0905a90965cbae113ec4d10f6f668d2
NylyxO <nylwei@outlook.com>
2024-03-28 10:31:42 +0000

    Add half-completed works

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,13 @@
 const Post = (props) => {
-  return <article key={props.post._id}>{props.post.message}</article>;
+  return (
+  <div className="container">
+    <div className="row">
+      <col-2>
+  <article key={props.post._id}>{props.post.message}</article>
+  </col-2>
+  </div>
+  </div>
+  );
 };
 
 export default Post;
