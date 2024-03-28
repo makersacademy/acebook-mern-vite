62357c6a08a4058ae624cf2488384b9841047065
jamie Young <jamieyoung0502@gmail.com>
2024-03-28 10:58:45 +0000

    tidying up CreatePostPage

--- frontend/src/pages/Post/CreatePostPage.jsx
+++ frontend/src/pages/Post/CreatePostPage.jsx
@@ -3,13 +3,16 @@ import { useNavigate } from "react-router-dom";
 import { createPosts } from "../../services/posts"; 
 export const CreatePostPage = () => {
     const [message, setMessage] = useState("");
-    // const [user, setUser] = useState("");
     const navigate = useNavigate();
     const token = localStorage.getItem("token");
 
     const handleSubmit = async (event) => {
       if(message == "") {
          alert("message field required")
+      }
+      else if (message.length < 20)
+      {
+        alert("message is too short")
       }
       else {
         event.preventDefault();
@@ -25,42 +28,14 @@ export const CreatePostPage = () => {
         <>
           <h2>Create a Post</h2>
           <form onSubmit={handleSubmit}>
-            <label htmlFor="text">Create a Post:</label>
+            <label htmlFor="message">Create a Post:</label>
             <input
-              defaultValue={"hello"}
               id="message"
               type="text"
               value={message}
               onChange={handleMessageChange}
             />
-            {/* <label htmlFor="User">User:</label>
-            <input
-              id="User"
-              type="text"
-              value={user}
-              onChange={handleUserChange}
-            /> */}
             <input role="submit-button" id="submit" type="submit" value="Submit" />
           </form>
         </>
       );}
-    
-  //     const token = localStorage.getItem("token");
-  //     if (token) {
-  //       createPosts(token, message)
-  //         .then((data) => {
-  //           console.log("redirecting...:");
-  //         localStorage.setItem("token", data.token);
-  //         })
-  //         .then(navigate("/posts"))
-            
-          
-  //         .catch((err) => {
-  //           console.error(err);
-  //         });
-  //     }
-  // if (!token) {
-  //   navigate("/login");
-  //   return;
-  // }
-
