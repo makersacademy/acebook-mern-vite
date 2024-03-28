a84d3a5e60e63632ac26229919b3614fa2112912
NylyxO <nylwei@outlook.com>
2024-03-28 11:16:06 +0000

    Start work on post layout. Add icon logic to button prop

--- frontend/src/components/Navbar/Buttons.jsx
+++ frontend/src/components/Navbar/Buttons.jsx
@@ -1,6 +1,9 @@
 export const NavButton = (props) => {
   return (
     <li className="nav-item">
+      <i className="display-icon">
+        {props.iconId}
+      </i>
       <a className="nav-link lightest-text-color" href="#">
         {props.linkName}
       </a>
--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -2,12 +2,28 @@ const Post = (props) => {
   return (
   <div className="container">
     <div className="row">
-      <col-2>
-  <article key={props.post._id}>{props.post.message}</article>
-  </col-2>
-  </div>
+      <div className="col-3"></div>
+      <div className="col-6">
+      <article key={props.post._id}>{props.post.user} posted at {props.post.createdDate}</article>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-3"></div>
+      <div className="col-6 ">
+      <article key={props.post._id}>{props.post.message}</article>
+      </div>
+    </div>
+    <div className="row">
+    <div className="col-6"></div>
+      <div className="col-6">
+        <div className="p">Likes:</div>
+      <article key={props.post._id}>{props.post.likes}</article>
+      </div>
+    </div>
   </div>
   );
 };
+
+
 
 export default Post;
