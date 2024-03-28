66933e940bc871f30b3ef8cca9c8b50803fa6bcd
Maria Jose <71413078+Mhernandel@users.noreply.github.com>
2024-03-28 10:19:10 +0000

    Update Post.jsx

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,11 +1,5 @@
 const Post = (props) => {
-  return (
-    <article key={props.post._id}>
-      <p>Username: {props.post.username}</p>
-      <p>Liked Counter: {props.post.likeCounter}</p>
-      <p>Message: {props.post.message}</p>
-    </article>
-  );
+  return <article key={props.post._id}>{props.post.message}</article>;
 };
 
 export default Post;
