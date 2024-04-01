2367534fa6f7acd1eef0f96accab447ac8f3e626
Benjamin Pearl <ben.pearl@btinternet.com>
2024-04-01 18:45:52 +0100

    jesus

--- api/models/post.js
+++ api/models/post.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 // A Schema defines the "shape" of entries in a collection. This is similar to
 // defining the columns of an SQL Database.
 const PostSchema = new mongoose.Schema({
+  fullName: { type: String },
   message: { type: String, required: true },
   liked: { type: Boolean, default: false },
   likeCounter: { type: Number, default: 0 },
-  image: { type: String },
-  createdAt: { type: Date, default: Date.now },
-  user: {
+  image: {type: String},
+  createdAt:{type: Date, default:Date.now},
+  user:{
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // Reference to the User model
-  },
+    ref: "User" // Reference to the User model
+    },    
 });
 
 // We use the Schema to create the Post model. Models are classes which we can
