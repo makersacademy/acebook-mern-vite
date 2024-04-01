b432aa3c23da544325239da427aa52f883087921
Benjamin Pearl <ben.pearl@btinternet.com>
2024-04-01 19:29:20 +0100

    matching

--- api/models/post.js
+++ api/models/post.js
@@ -22,13 +22,13 @@ const Post = mongoose.model("Post", PostSchema);
 // These lines will create a test post every time the server starts.
 // You can delete this once you are creating your own posts.
 const dateTimeString = new Date().toLocaleString("en-GB");
-new Post({
-  message: "test message",
-  liked: true,
-  likeCounter: 1,
-  image: "https://example.com/image.jpg",
-  user: "66018bc48a87746692806ab5",
-  createdAt: new Date(),
-}).save();
+new Post({ 
+          message: 'update model post',
+          liked: true,
+          likeCounter: 1,
+          image:"https://example.com/image.jpg",
+          user: "66018bc48a87746692806ab5",
+          createdAt: new Date()
+          }).save();
 
 module.exports = Post;
