8f2e5406cd872ee69b4f342fa5bb5533a0598020
Nadia Bourial <nadia.bourial@gmail.com>
2024-04-02 13:15:52 +0100

    added tests for time ago

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -3,29 +3,31 @@ import { format } from 'date-fns';
 const Post = (props) => {
 
 
-    const postDateTime = Date(props.post.post_date);
+  
    
   
     const howLongAgo = () => {
       const postDateTime = new Date(props.post.post_date);
+      const formatedPostDateTime = format(postDateTime, "do MMMM yyyy, HH:mm");
       const now = new Date();
       const timeDifferenceSeconds = (now.getTime() - postDateTime.getTime())/1000;
       const timeDifferenceMins = Math.round(timeDifferenceSeconds /60);
       const timeDifferenceHours = Math.round(timeDifferenceMins /60);
-      if (timeDifferenceMins < 1 ) {
+      if (timeDifferenceSeconds < 60 ) {
         return "Less than a minute ago"
       } else if (timeDifferenceMins < 60) {
         return `${timeDifferenceMins} minutes ago`
       } else if (timeDifferenceHours < 24) {
         return `${timeDifferenceHours} hours ago`
       } else {
-        return format(postDateTime, "do MMMM yyyy, HH:mm")
+        return formatedPostDateTime;
       }
     }
 
-  return <article key={props.post._id}>{props.post.message}
+  return <article key={props.post._id}>
+    <p data-testId = "message"> {props.post.message}</p>
     
-    <p>{howLongAgo()}</p>
+    <p data-testId = "time-ago">{howLongAgo()}</p>
   </article>;
 
 };
--- frontend/tests/components/post.test.jsx
+++ frontend/tests/components/post.test.jsx
@@ -2,15 +2,48 @@ import { render, screen } from "@testing-library/react";
 
 import Post from "../../src/components/Post/Post";
 
-
+import { sub } from 'date-fns';
 
 describe("Post", () => {
   test("displays the message as an article", () => {
-    const testPost = { _id: "123", message: "test message" };
+    const testPost = { _id: "123", message: "test message", post_date: '2024-04-02T09:40:18.010Z' };
     render(<Post post={testPost} />);
 
-    const article = screen.getByRole("article");
-    expect(article.textContent).toBe("test message");
+    const message = screen.getByTestId("message");
+    expect(message.textContent).toContain("test message");
+  });
+
+  test("displays the post date - less than a minute ago", () => {
+    const dateNow  = new Date();
+    const thirtySecsAgo = sub(dateNow, {seconds: 30});
+    const testPost = { _id: "123", message: "test message", post_date: thirtySecsAgo };
+    render(<Post post={testPost} />);
+    const timeAgo = screen.getByTestId("time-ago");
+    expect(timeAgo.textContent).toBe("Less than a minute ago");
+  });
+
+  test("displays the post date 10 minutes ago", () => {
+    const dateNow  = new Date();
+    const thirtySecsAgo = sub(dateNow, {minutes: 10});
+    const testPost = { _id: "123", message: "test message", post_date: thirtySecsAgo };
+    render(<Post post={testPost} />);
+    const timeAgo = screen.getByTestId("time-ago");
+    expect(timeAgo.textContent).toBe("10 minutes ago");
+  });
+  test("displays the post date 10 hours ago", () => {
+    const dateNow  = new Date();
+    const thirtySecsAgo = sub(dateNow, {hours: 10});
+    const testPost = { _id: "123", message: "test message", post_date: thirtySecsAgo };
+    render(<Post post={testPost} />);
+    const timeAgo = screen.getByTestId("time-ago");
+    expect(timeAgo.textContent).toBe("10 hours ago");
+  });
+
+  test("displays a specific post date over a day ago", () => {
+    const testPost = { _id: "123", message: "test message", post_date: "2024-04-01T09:40:18.010Z" };
+    render(<Post post={testPost} />);
+    const timeAgo = screen.getByTestId("time-ago");
+    expect(timeAgo.textContent).toBe("1st April 2024, 10:40");
   });
 
 });
