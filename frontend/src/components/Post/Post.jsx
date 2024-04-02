779484d48f64248c86376b8a50a3dc35730efac8
Nadia Bourial <nadia.bourial@gmail.com>
2024-04-02 12:49:10 +0100

    added time ago to comments

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,33 @@
+import { format } from 'date-fns';
+
 const Post = (props) => {
-  return <article key={props.post._id}>{props.post.message}</article>;
+
+
+    const postDateTime = Date(props.post.post_date);
+   
+  
+    const howLongAgo = () => {
+      const postDateTime = new Date(props.post.post_date);
+      const now = new Date();
+      const timeDifferenceSeconds = (now.getTime() - postDateTime.getTime())/1000;
+      const timeDifferenceMins = Math.round(timeDifferenceSeconds /60);
+      const timeDifferenceHours = Math.round(timeDifferenceMins /60);
+      if (timeDifferenceMins < 1 ) {
+        return "Less than a minute ago"
+      } else if (timeDifferenceMins < 60) {
+        return `${timeDifferenceMins} minutes ago`
+      } else if (timeDifferenceHours < 24) {
+        return `${timeDifferenceHours} hours ago`
+      } else {
+        return format(postDateTime, "do MMMM yyyy, HH:mm")
+      }
+    }
+
+  return <article key={props.post._id}>{props.post.message}
+    
+    <p>{howLongAgo()}</p>
+  </article>;
+
 };
 
 export default Post;
--- frontend/tests/components/post.test.jsx
+++ frontend/tests/components/post.test.jsx
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 
 import Post from "../../src/components/Post/Post";
 
+
+
 describe("Post", () => {
   test("displays the message as an article", () => {
     const testPost = { _id: "123", message: "test message" };
@@ -10,4 +12,5 @@ describe("Post", () => {
     const article = screen.getByRole("article");
     expect(article.textContent).toBe("test message");
   });
+
 });
