d868dd252178e16592bec89e1bbca3eb3fc503f4
mustafa yahya <mustafayahya97@outlook.com>
2024-04-02 10:39:59 +0100

    display date on posts and order by posts

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -3,8 +3,14 @@ const { generateToken } = require("../lib/token");
 
 const getAllPosts = async (req, res) => {
   const posts = await Post.find();
+  
+  const sorted_posts = await posts.sort((a,b) => {
+    return new Date(a.createdDate).getTime() -
+    new Date(b.createdDate).getTime()
+  }).reverse();
+  console.log(posts)
   const token = generateToken(req.user_id);
-  res.status(200).json({ posts: posts, token: token });
+  res.status(200).json({ posts: sorted_posts, token: token });
 };
 
 const createPost = async (req, res) => {
--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,20 @@
+
+
 const Post = (props) => {
-  return <article key={props.post._id}>{props.post.message}</article>;
-};
+  const date = new Date(props.post.createdDate);
+
+  const formattedDate = date.toLocaleString('en-UK', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true 
+  });
+  return(<div>
+    <article key={props.post._id}>{props.post.message}</article>
+    <p>posted: {formattedDate}</p>
+    <p>likes: {props.post.likes}</p>
+    </div> )}
 
 export default Post;
