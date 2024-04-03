f5dcde9984cffa857b05c06a51e3dee16347a77e
mustafa yahya <mustafayahya97@outlook.com>
2024-04-03 15:24:08 +0100

    post modified

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,7 +1,9 @@
 
-const Post = (props) => {
-  const date = new Date(props.post.createdDate);
+import { ToggleLike } from "./like";
 
+
+export const Post = (props) => {
+  const date = new Date(props.post.createdDate);
   const formattedDate = date.toLocaleString('en-UK', {
     month: 'long',
     day: 'numeric',
@@ -10,21 +12,12 @@ const Post = (props) => {
     minute: 'numeric',
     hour12: true 
   });
-  return(<div>
-    <article key={props.post._id}>{props.post.message}</article>
-    <p>posted: {formattedDate}</p>
-    <p>likes: {props.post.likes}</p>
-    </div> )}
-  import { ToggleLike } from "./like";
-
-
-export const Post = (props) => {
   return (
   <div className="container">
     <div className="row">
       <div className="col-3"></div>
       <div className="col-6">
-      <article aria-label="Post Header" key={props.post._id}>{props.post.user} posted at {props.post.createdDate}</article>
+      <article aria-label="Post Header" key={props.post._id}>{props.post.user} posted at {formattedDate}</article>
       </div>
       <div className="col-1" aria-label="Number of likes">
         <ToggleLike isLiked={false} />
