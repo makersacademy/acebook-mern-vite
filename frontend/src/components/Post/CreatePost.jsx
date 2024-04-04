c657d55da1fcd63f6b0e12804c05dd2db56c75a9
Amy Brown <145221172+AmySpektrix@users.noreply.github.com>
2024-04-04 10:31:32 +0100

    mini fix 1 (#19)

--- frontend/src/components/Post/CreatePost.jsx
+++ frontend/src/components/Post/CreatePost.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { createPosts } from "../../services/posts";
-import LikeDislike from "../Likes/LikeCounter"
+// import LikeDislike from "../Likes/LikeCounter"
 import UploadWidget from './UploadWidget';
  
 const CreatePost = (props) => {
