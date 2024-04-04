d4803a9f7051e62086abd5fb71654348dbc21164
Jason <jason.sayeed@gmail.com>
2024-04-04 09:54:36 +0100

    fixed 3/4 of andre's PR

--- api/models/user.js
+++ api/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   fullName: { type: String, required: true },
   profilePicture: { type: String, required: true },
