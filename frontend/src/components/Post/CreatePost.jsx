5aec74eb5ceb3a663de5965969509a422421fbe7
EPedley <lizzypedley@gmail.com>
2024-04-05 11:13:23 +0100

    adding post image preview

--- frontend/src/components/Post/CreatePost.jsx
+++ frontend/src/components/Post/CreatePost.jsx
@@ -4,6 +4,11 @@ import { createPosts } from "../../services/posts";
 // import LikeDislike from "../Likes/LikeCounter"
 import UploadWidget from './UploadWidget';
 import '../../css/CreatePost.css'
+import { Cloudinary} from "@cloudinary/url-gen";
+import {AdvancedImage} from '@cloudinary/react';
+import {fill} from "@cloudinary/url-gen/actions/resize";
+const CLOUD_NAME = import.meta.env.VITE_CLOUD_NAME
+
  
 const CreatePost = (props) => {
     const token = localStorage.getItem("token")
@@ -11,14 +16,18 @@ const CreatePost = (props) => {
     const [imageField, setImageField] = useState("")
     const navigate = useNavigate();
     const [error, setError] = useState([])
+    const cld = new Cloudinary({cloud: {cloudName: CLOUD_NAME}});
+    const [imagePreview, setImagePreview] = useState('')
 
     const handleMessageChange = (event) => {
         setMessageField(event.target.value)
     }; 
 
     const handleImageUpload = (imageLocation) => {
-        console.log('IM IN HANDLE IMAGE UPLOAD')
-        setImageField(imageLocation)
+        setImageField(imageLocation);
+        setImagePreview(cld.image(imageLocation));
+        // imagePreview.resize(fill().width(250).height(250));
+        console.log(imagePreview)
     }
 
     const handleSubmit = async (event) => {
@@ -27,6 +36,7 @@ const CreatePost = (props) => {
             await createPosts(token, messageField, imageField);
             setImageField("");
             setMessageField(""); 
+            setImagePreview('')
             props.onCreatePost();
         } 
         catch (err) {
@@ -43,6 +53,8 @@ const CreatePost = (props) => {
 
           <input data-testid="post-message" type='text' value={messageField} onChange={handleMessageChange}></input>
           <UploadWidget folder={'posts'} buttonText = {'Add a photo'} handleImageUpload={handleImageUpload}/>
+          {imagePreview && <AdvancedImage style={{ height: "250px", width: "250px", objectFit: "cover"}} cldImg={imagePreview} />}
+          {/* <div>{imagePreview}</div> */}
           <input className="post-button" role="submit-button" id="submit" type="submit" value="Submit" />
         </form>
         <div>
