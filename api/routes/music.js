350c092ef66c996ebf87d6b0cc628424431969b9
katrinamichanicou <148572989+katrinamichanicou@users.noreply.github.com>
2024-04-24 16:48:57 +0100

    routes and services added

--- api/routes/music.js
+++ api/routes/music.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const MusicController = require("../controllers/music");
 
 router.get("/genre", MusicController.getGenres);
+router.get("/genre/:id/artists", MusicController.getArtistsForGenre);
+router.get("/artist/:id/top", MusicController.getTopTracksForArtist);
+router.get("/track/:id", MusicController.getTrack);
 
 module.exports = router;
--- api/tests/controllers/music.test.js
+++ api/tests/controllers/music.test.js
@@ -1,12 +1,15 @@
-const request = require("supertest");
-const app = require("../../app");
+// const request = require("supertest");
+// const app = require("../../app");
 
+// TODO: Tests to be added. Need to research mocking fetch with Jest
+// test below actually calls Deezer API
+// - Aakash / Katrina
 
-describe("GET, genres", () => {
-    test("the response code is 200", async () => {
+// describe("GET, genres", () => {
+//     test("the response code is 200", async () => {
+    
+//     const response = await request(app)
+//     .get("/music/genre")
 
-    const response = await request(app)
-    .get("/music/genre")
-
-    expect(response.status).toEqual(200);
-})});
+//     expect(response.status).toEqual(200);
+// })});
