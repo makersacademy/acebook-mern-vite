e528d10ca013a76e8c858a3c3beec9b3b3a16a91
Bobby Chaudhry <bobbychaudhry@hotmail.co.uk>
2024-04-24 12:49:55 +0100

    audio button component created and css styling

---
+++ frontend/src/components/AudioButton/AudioButton.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./AudioButton.css";
+
+const AudioButton = ( {trackPreview} ) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    
+    const handleClick = () => {
+    setIsPlaying(!isPlaying);
+    playPause()
+  };
+
+  function playPause() {
+    var myAudio = document.getElementById("ASong");
+    if (myAudio.paused) {
+      myAudio.play();
+    } else {
+      myAudio.pause();
+    }
+  }
+
+
+  return (
+      <div className="audio">
+      <button className={`play-button ${isPlaying ? 'pulsate' : ''}`} onClick={handleClick}>
+        <span className="icon">{isPlaying ? "❚❚" : "▶"}</span> 
+      </button>
+        <audio id="ASong" src={trackPreview} hidden />
+      </div>
+  );
+}
+
+export default AudioButton
+
+
+
+
+
