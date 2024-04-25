73001fe3a0c2d0f540af48ac968dec4c85b99d62
Bobby Chaudhry <bobbychaudhry@hotmail.co.uk>
2024-04-25 17:43:46 +0100

    Answer component created and renders on Quizpage with showing if is correct on click

--- frontend/src/components/Answer/Answer.jsx
+++ frontend/src/components/Answer/Answer.jsx
@@ -1,17 +1,35 @@
-import React from "react"
+import { useState } from "react";
+
+const Answer = ({ selectedTrack, shuffledArtistAnswerList }) => {
+    const [score, setScore] = useState(0);
+    // console.log(selectedTrack.artist)
+    const answerClick = (artist) => {
+        const isCorrect = selectedTrack.artist === artist
+        console.log(isCorrect)
+        if (isCorrect) {
+            setScore(score + 1);
+        }
+    };
 
 
-const Answer = ( { shuffledArtistAnswerList }) => {
+
     return (
+        <>
         <div>
         {shuffledArtistAnswerList.map((artist, id) => (
             <ul key={id}>
-                <button>{artist}</button>
+                <button onClick={() => answerClick(artist)}>{artist}</button>
             </ul>
         ))}
         </div>
+        <div>
+            <h1 className="results-header">Results</h1>
+            <p>Your Score: {score}</p>
+        </div>
+        </>
     )
 };
+
 
 export default Answer;
 
