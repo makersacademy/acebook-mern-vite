b6946e916c1d03bb0259730ebffb0865cdc1ff65
Jason <jason.sayeed@gmail.com>
2024-04-25 15:19:08 +0100

    tests for question component and quiz page

--- frontend/tests/components/question.test.jsx
+++ frontend/tests/components/question.test.jsx
@@ -3,7 +3,7 @@ import { screen, render } from "@testing-library/react";
 import Question from "../../src/components/Question/Question";
 
 describe("Question component", () => {
-  test("Question displays on the page", () => {
+  test("Question is generated", () => {
     render(<Question questionType="artist" />);
     expect(screen.getByText("What is the name of the artist?")).toBeTruthy();
   });
--- frontend/tests/pages/quizPage.test.jsx
+++ frontend/tests/pages/quizPage.test.jsx
@@ -1,17 +1,33 @@
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { screen, render, fireEvent } from "@testing-library/react";
+// import { BrowserRouter } from "react-router-dom";
 import { QuizPage } from "../../src/pages/Quiz/QuizPage";
 
-describe("Question", () => {
-  test("displays the message correctly", () => {
-    render(
-      <BrowserRouter>
-        <QuizPage />
-      </BrowserRouter>
-    );
-    const elementWithMessage = screen.getByText(
-      "What is the name of the artist?"
-    );
-    expect(elementWithMessage).toBeInTheDocument();
+describe("Audio button component", () => {
+  test("Play button is accessible on the page", () => {
+    render(<QuizPage />);
+    const playButton = screen.getByRole("button");
+    expect(playButton.textContent).toBe("▶");
+  });
+
+  test("Pause button is accessible on the page", () => {
+    render(<QuizPage />);
+    const playButton = screen.getByRole("button");
+    fireEvent.click(playButton);
+    expect(playButton.textContent).toBe("❚❚");
+  });
+
+  test("AudioButton changes state to play when clicked", () => {
+    render(<QuizPage />);
+    const playButton = screen.getByRole("button");
+    fireEvent.click(playButton);
+    fireEvent.click(playButton);
+    expect(playButton.textContent).toBe("▶");
+  });
+});
+
+describe("Question component", () => {
+  test("Question displays on page", () => {
+    render(<QuizPage />);
+    expect(screen.getByText("What is the name of the artist?")).toBeTruthy();
   });
 });
