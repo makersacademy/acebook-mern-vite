196156f068b4b40bcf196e53c801d1fcdba426af
katrinamichanicou <148572989+katrinamichanicou@users.noreply.github.com>
2024-04-26 18:21:48 +0100

    testing added and passes. testing-library/jest-dom dependency added to help with testing. This fixed the chai error, which previously occurred when using .toBeInTheDocument()

--- frontend/tests/pages/homePage.test.jsx
+++ frontend/tests/pages/homePage.test.jsx
@@ -1,40 +1,50 @@
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
 import { BrowserRouter } from "react-router-dom";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { HomePage } from "../../src/pages/Home/HomePage";
 
 describe("Home Page", () => {
-  test("welcomes you to the site", () => {
+  test("Tagline is displayed", () => {
     // We need the Browser Router so that the Link elements load correctly
     render(
       <BrowserRouter>
+      <GoogleOAuthProvider clientId="108211060185-rc1g9je54b5hsufug7fho331oerfrpk2.apps.googleusercontent.com">
         <HomePage />
+      </GoogleOAuthProvider>
       </BrowserRouter>
     );
 
-    const heading = screen.getByRole("heading");
-    expect(heading.textContent).toEqual("Welcome to Acebook!");
+    const tagline = screen.getByRole("heading");
+    expect(tagline.textContent).toEqual("Let's get Kwizical!");
   });
 
-  test("Displays a signup link", async () => {
+  test("Displays 'play as guest' link", async () => {
     render(
       <BrowserRouter>
+      <GoogleOAuthProvider clientId="108211060185-rc1g9je54b5hsufug7fho331oerfrpk2.apps.googleusercontent.com">
         <HomePage />
+      </GoogleOAuthProvider>
       </BrowserRouter>
     );
 
-    const signupLink = screen.getByText("Sign Up");
-    expect(signupLink.getAttribute("href")).toEqual("/signup");
+    const playAsGuest = screen.getByText("Play as guest");
+    expect(playAsGuest.getAttribute("href")).toEqual("/kwizical");
   });
 
-  test("Displays a login link", async () => {
+  test("Displays a Google link", async () => {
     render(
       <BrowserRouter>
+      <GoogleOAuthProvider clientId="108211060185-rc1g9je54b5hsufug7fho331oerfrpk2.apps.googleusercontent.com">
         <HomePage />
+      </GoogleOAuthProvider>
       </BrowserRouter>
     );
 
-    const loginLink = screen.getByText("Log In");
-    expect(loginLink.getAttribute("href")).toEqual("/login");
+    const googleLoginButton = screen.getByRole("button", {
+      name: /sign in with google to play/i,
+    });
+    expect(googleLoginButton).toBeInTheDocument();
+    expect(googleLoginButton).toHaveTextContent("Sign in with Google to play");
   });
 });
