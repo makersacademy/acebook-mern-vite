8948f14dfad5d85ad8f84aa8039bd5ad472786ef
katrinamichanicou <148572989+katrinamichanicou@users.noreply.github.com>
2024-04-26 15:26:39 +0100

    buttonColors array issues corrected

--- frontend/src/components/Answer/Answer.jsx
+++ frontend/src/components/Answer/Answer.jsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 
 const Answer = ({ selectedTrack, shuffledArtistAnswerList }) => {
     const [score, setScore] = useState(0);
-    const [buttonColors, setButtonColors] = useState('bg-blue-500');
+    const [buttonColors, setButtonColors] = useState(new Array(4).fill("bg-blue-500"));
 
     // console.log(selectedTrack.artist)
     const answerClick = (artist, id) => {
         const isCorrect = selectedTrack.artist === artist
-        const newButtonColors = [buttonColors];
-        console.log(isCorrect)
+        const newButtonColors = [...buttonColors];
         if (isCorrect) {
             setScore(score + 1);
             newButtonColors[id] = 'bg-green-500';
@@ -30,7 +29,7 @@ const Answer = ({ selectedTrack, shuffledArtistAnswerList }) => {
                 w-64 bg-blue-500 text-white py-4 px-4 rounded-xl font-bold uppercase
                 before:block before:absolute before:h-full before:w-full
                 before:left-0 before:top-0 before:-translate-y-full before:transition-transform ${buttonColors[id]}
-                ${buttonColors === 'bg-blue-500' ? 'hover:bg-blue-300' : ''}`}
+                ${buttonColors[id] === 'bg-blue-500' ? 'hover:bg-blue-300' : ''}`}
                 style={{ marginBottom: '10px' }}
                 >{artist}</button>
             </ul>
