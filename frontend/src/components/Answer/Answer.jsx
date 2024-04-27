82c656ea21e1cdd0d8c598cd9e3097d84030b278
katrinamichanicou <148572989+katrinamichanicou@users.noreply.github.com>
2024-04-27 16:21:35 +0100

    update genrepicker buttons

--- frontend/src/components/Answer/Answer.jsx
+++ frontend/src/components/Answer/Answer.jsx
@@ -1,48 +1,47 @@
 import { useState } from "react";
 
 const Answer = ({ selectedTrack, shuffledArtistAnswerList }) => {
-    const [score, setScore] = useState(0);
-    const [buttonColors, setButtonColors] = useState(new Array(4).fill("bg-blue-500"));
+  const [score, setScore] = useState(0);
+  const [buttonColors, setButtonColors] = useState(new Array(4).fill("bg-blue-500")); // creates an array of button colours, set to blue initially
 
-    // console.log(selectedTrack.artist)
-    const answerClick = (artist, id) => {
-        const isCorrect = selectedTrack.artist === artist
-        const newButtonColors = [...buttonColors];
-        if (isCorrect) {
-            setScore(score + 1);
-            newButtonColors[id] = 'bg-green-500';
-        } else {
-            newButtonColors[id] = 'bg-red-500';
-        }
-        setButtonColors(newButtonColors);
-    };
+  // console.log(selectedTrack.artist)
+  const answerClick = (artist, id) => {
+    const isCorrect = selectedTrack.artist === artist
+    const newButtonColors = [...buttonColors];
+    if (isCorrect) {
+      setScore(score + 1);
+      newButtonColors[id] = 'bg-green-500';
+    } else {
+      newButtonColors[id] = 'bg-red-500';
+    }
+    setButtonColors(newButtonColors);
+  };
 
 
-    return (
-        <>
-        <div>
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-y-2 md:grid-cols-2 gap-x-40"> {/* 'grid grid-cols-2' this turns the row of answers into two columns. Adding md: applies the changes only when the screen is wider than the md breakpoint*/}
         {shuffledArtistAnswerList.map((artist, id) => (
-            <ul key={id}>
-                <button 
-                onClick={() => answerClick(artist, id)} 
-                className={`btn overflow-hidden relative
-                w-64 bg-blue-500 text-white py-4 px-4 rounded-xl font-bold uppercase
+            <button
+              key={id}
+              onClick={() => answerClick(artist, id)}
+              className={`btn overflow-hidden relative
+                w-64 text-white py-4 px-4 rounded-xl font-bold uppercase
                 before:block before:absolute before:h-full before:w-full
-                before:left-0 before:top-0 before:-translate-y-full before:transition-transform ${buttonColors[id]}
-                ${buttonColors[id] === 'bg-blue-500' ? 'hover:bg-blue-300' : ''}`}
-                style={{ marginBottom: '10px' }}
-                >{artist}</button>
-            </ul>
+                before:left-0 before:top-0 before:-translate-y-full before:transition-transform ${buttonColors[id]} 
+                ${buttonColors[id] === 'bg-blue-500' ? 'hover:bg-blue-300' : ''} 
+            `}>
+              {artist}
+            </button>
         ))}
-        </div>
-        <div>
-            <h1 className="results-header"
-            style={{ marginTop: '20px' }}>Results</h1>
-            <p>Your Score: {score}</p>
-        </div>
-        </>
-    )
- 
+      </div>
+      <div className="mt-8">
+        <h1 className="results-header text-2xl font-bold">Results</h1>
+        <p>Your Score: {score}</p>
+      </div>
+    </>
+  )
+
 };
 
 
--- frontend/src/components/GenrePicker/GenrePicker.jsx
+++ frontend/src/components/GenrePicker/GenrePicker.jsx
@@ -1,26 +1,33 @@
-const GenrePicker = ({onGenreSelect}) =>{
-    const genres = [
-        {id: 132, name: 'Pop'},
-        {id: 116, name: 'Rap/Hiphop'},
-        {id: 165, name: 'RnB'},
-        {id: 113, name: 'Dance'},
-        {id: 173, name: 'Films/Games'},
-        {id: 464, name: 'Metal'}
-    ]
+const GenrePicker = ({ onGenreSelect }) => {
+  const genres = [
+    { id: 132, name: 'Pop' },
+    { id: 116, name: 'Rap/Hiphop' },
+    { id: 165, name: 'RnB' },
+    { id: 113, name: 'Dance' },
+    { id: 173, name: 'Films/Games' },
+    { id: 464, name: 'Metal' }
+  ]
 
-return (
-    <>
-        <div>
-            <h2> Select a genre </h2>
-        </div>
-        <div> {genres.map(genre => (
-            <button key={genre.id} className={`btn overflow-hidden relative
-            w-64 bg-blue-500 text-white py-4 px-4 rounded-xl font-bold uppercase
-            before:block before:absolute before:h-full before:w-full
-            before:left-0 before:top-0 before:-translate-y-full hover:bg-blue-300`} onClick={()=>onGenreSelect(genre.id)}>{genre.name}</button>))}
-        </div>
-    </>
-)
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="mt-8">
+        <h1 className="text-4xl font-bold mb-8 text-center text-blue-500">
+          Select a genre
+        </h1>
+      </div>
+      <div className="grid grid-cols-3 gap-12 p-8">
+        {genres.map(genre => (
+          <button key={genre.id} className={`bg-blue-500 hover:bg-blue-300 text-white font-bold w-40 h-40 
+            rounded-lg shadow-md flex items-center justify-center transition duration-300 
+            ease-in-out transform hover:scale-105`}
+            onClick={() => onGenreSelect(genre.id)}
+          >
+            <span className="text-xl">{genre.name}</span>
+          </button>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default GenrePicker
