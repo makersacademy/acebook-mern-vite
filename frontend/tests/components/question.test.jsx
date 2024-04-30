4cb915262d1222e46ef5cecc37045187d0d890b8
Bobby Chaudhry <bobbychaudhry@hotmail.co.uk>
2024-04-30 16:05:12 +0100

    another file

--- frontend/tests/components/question.test.jsx
+++ frontend/tests/components/question.test.jsx
@@ -1,11 +1,26 @@
 import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 import Question from "../../src/components/Question/Question";
 
 describe("Question component", () => {
-  test("Question is generated", () => {
-    // render(<Question questionType="artist" />);
-    // expect(screen.getByText("What is the name of the artist?")).toBeTruthy();
-    expect(true).toBeTruthy()
+  test("Question is generated for track title", () => {
+    render(<Question questionType={0} />);
+    expect(screen.queryByText("What is the name of the track?")).toBeInTheDocument();
+  });
+
+  test("Question is generated for artist name", () => {
+    render(<Question questionType={1} />);
+    expect(screen.queryByText("What is the name of the artist?")).toBeInTheDocument();
+  });
+
+  test("Question is generated for album title", () => {
+    render(<Question questionType={2} />);
+    expect(screen.queryByText("What is the name of the album?")).toBeInTheDocument();
   });
 });
+
+
+
+
+
