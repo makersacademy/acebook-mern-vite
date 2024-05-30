0f2ea4b0638622d03992e8c8ee2a3d80b087b3ea
niamh <niamh321@me.com>
2024-05-30 14:40:37 +0100

    created css file for the post component, added toggle like/unlike to like button.

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,8 +1,19 @@
+import "../../../css/post.css"
+import { useState } from "react";
+
 const Post = (props) => {
 
-  return <div key={props.post._id}>
+  const [likeStatus, setLikeStatus] = useState(false)
+  // cont [likeCount, setLikeCount] = useState(0)
+  const handleLike = () => {
+    setLikeStatus(!likeStatus)
+  }
+
+  return <div key={props.post._id} className="post">
     <h2>{props.post.username} - {props.post.createdAt}</h2>
     <article>{props.post.message}</article>
+    <button onClick={ handleLike }>{likeStatus ? 'Unlike' : 'Like'}</button>
+    <p>_likes</p>
     </div>
 };
 
