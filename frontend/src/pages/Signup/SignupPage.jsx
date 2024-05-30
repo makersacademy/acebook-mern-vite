f9494c14062a87932d2f8600de8c7981cb3e8e13
Adrian <adrian.oakley@gmail.com>
2024-05-30 16:05:17 +0100

    Updated Sign-Up-Page look

--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -53,12 +53,6 @@ export const SignupPage = () => {
 
   return (
     <div>
-    <div className= "home">
-      <div className="SignUp-banner">
-        <h1>Acebook</h1>
-        <h3>Brought to you by the Agile Avengers</h3>
-      </div>
-    </div>
     <h2>Sign Up for Your Free Account!</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name:</label>
