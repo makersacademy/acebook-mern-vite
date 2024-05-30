cce88b21bee100233ec4af671f70d7b164df7a0d
Jetheesan <jetheesan@gmail.com>
2024-05-30 14:13:58 +0100

    implement the sanitisation feature on the login page

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { login } from "../../services/authentication";
+import DOMpurify from "dompurify";
+
 
 export const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -10,12 +12,18 @@ export const LoginPage = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const CleanEmail = DOMpurify.sanitize(email)
+    const CleanPassword = DOMpurify.sanitize(password)
+
+
+
     try {
-      const token = await login(email, password);
+      const token = await login(CleanEmail, CleanPassword);
       localStorage.setItem("token", token);
       navigate("/posts");
     } catch (err) {
       console.error(err);
+      console.log({CleanEmail})
       navigate("/login");
     }
   };
@@ -48,6 +56,7 @@ export const LoginPage = () => {
         />
         <input role="submit-button" id="submit" type="submit" value="Submit" />
       </form>
+      {/* <script type="text/javascript" src="dist/purify.min.js"></script> */}
     </>
   );
 };
