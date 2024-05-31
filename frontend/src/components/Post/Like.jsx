aa5889e25210dae30f1f464ee224f1e65d7476ee
Jack <jackawmurray@hotmail.com>
2024-05-31 13:05:54 +0100

    Final iteration of liked componant

--- frontend/src/components/Post/Like.jsx
+++ frontend/src/components/Post/Like.jsx
@@ -4,14 +4,11 @@ import { changeLike } from "../../services/posts";
 const Like = (props) => {
   const token = localStorage.getItem("token");
   const user_id = localStorage.getItem("user_id");
-  console.log(token);
-  const [liked, setLiked] = useState(false);
-  console.log(liked)
+  let liked = props.post.like_array.includes(user_id);
 
   const likePost = () => {
-    let liked_state = !liked;  // necessary as this code runs faster than setLiked lol see "race condition"
-    setLiked(!liked);
-    changeLike(token, props.post._id, user_id, liked_state)
+    liked = !liked;
+    changeLike(token, props.post._id, user_id, liked)
     .catch((err) => {console.error(err)});
     props.update(!props.value);
   };
