b5b18d81850c92ac5c98e002c059405991f2c658
Anko21 <am.kontaratou@gmail.com>
2024-06-03 18:57:53 +0100

    fixed the error at SignUp Page

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -66,7 +66,6 @@ const { generateToken } = require("../lib/token");
 const createToken = async (req, res) => {
   const { email, password } = req.body;
 
-  try {
     const user = await User.findOne({ email });
     if (!user) {
       console.log("Auth Error: User not found");
@@ -81,11 +80,8 @@ const createToken = async (req, res) => {
 
     const token = generateToken(user.id);
 
-/////////////////////////////////////////////////////////////////////////////////////////
     res.status(201).json({ token: token, user_id: user._id, message: "OK" });
-
   }
-};
 
 const signup = async (req, res) => {
   const { firstName, lastName, email, password, DOB } = req.body;
--- frontend/src/services/authentication.js
+++ frontend/src/services/authentication.js
@@ -2,61 +2,61 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const login = async (email, password) => {
-  const payload = {
-    email: email,
-    password: password,
-  };
+    const payload = {
+        email: email,
+        password: password,
+    };
 
-  const requestOptions = {
+    const requestOptions = {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  };
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+    };
 
-  const response = await fetch(`${BACKEND_URL}/tokens`, requestOptions);
+    const response = await fetch(`${BACKEND_URL}/tokens`, requestOptions);
 
   // Check if the response is successful
-  if (response.ok) {
-    let data = await response.json();
-
-    return { token: data.token, user_id: data.user_id }
-
-  } else {
+    if (response.ok) {
+        let data = await response.json();
+        return { token: data.token, user_id: data.user_id }
+    } else {
     // Throw an error if the response status is not OK
     throw new Error(
-      `Received status ${response.status} when logging in. Expected 200 OK`
+        `Received status ${response.status} when logging in. Expected 200 OK`
     );
-  }
+    }
 };
+
+
 export const signup = async ({ firstName, lastName, email, password, DOB, gender }) => {
-  const payload = {
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: password,
-    DOB: DOB,
-    gender: gender
-  };
+    const payload = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        DOB: DOB,
+        gender: gender
+    };
 
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  };
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+    };
 
-  let response = await fetch(`${BACKEND_URL}/users`, requestOptions);
-
+    let response = await fetch(`${BACKEND_URL}/users`, requestOptions);
+console.log(response);
   // Check if the response is successful
-  if (response.ok) {
-    return;
-  } else {
+    if (response.ok) {
+        return;
+    } else {
     // Throw an error if the response status is not OK
     throw new Error(
-      `Received status ${response.status} when signing up. Expected 200 OK`
+        `Received status ${response.status} when signing up. Expected 200 OK`
     );
-  }
+    }
 };
