265cfc043e5c5ca169ee76603abbaa95f5a361ae
Robert McIsaac <robmcisaac@hotmail.co.uk>
2024-10-02 12:43:41 +0100

    createPost front end request implemented

---
+++ frontend/src/components/CreatePostForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import { createPost } from "../services/posts";
+
+
+const CreatePostForm = () => {
+    
+    const [message, setMessage] = useState("");
+
+    const handleMessageChange = (event) => setMessage(event.target.value);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`message= ${message}`)
+        createPost(message);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="message">Enter message:</label>
+            <input 
+                type="text"
+                name="message"
+                value={message}
+                onChange={handleMessageChange} 
+            />
+
+            <input 
+                type="submit"
+                value="Submit" 
+            />
+        </form>
+    )
+};
+
+export default CreatePostForm;
--- frontend/src/services/posts.js
+++ frontend/src/services/posts.js
@@ -18,3 +18,36 @@ export async function getPosts(token) {
   const data = await response.json();
   return data;
 }
+
+// CREATE POST ("POST")
+export async function createPost(message) {
+  
+  // payload object is created, which contains the message data that will be sent to the backend. The key is message, and the value is the argument passed to the function.
+  // organizes the data that needs to be sent to the backend. The backend typically expects data in a specific format, and payload ensures that.
+  const payload = {
+    message: message
+  }
+  
+  const requestOptions = {
+    method: "POST",
+    // headers includes an Authorization header (using the token from localStorage) and Content-Type: "application/json"
+    headers: {
+      Authorization: `Bearer ${localStorage.token}`,
+      // The content type is essential because it tells the server that the request body contains JSON.
+      "Content-Type": "application/json"
+    },
+    // JSON.stringify(payload) converts the JavaScript object to JSON, the format most APIs expect in a POST request body.
+    body: JSON.stringify(payload)
+  };
+
+  // Sends a POST request to the /posts endpoint using the requestOptions. The server will create a new post with the data in payload.
+  const response = await fetch(`${BACKEND_URL}/posts`, requestOptions);
+
+  if (response.status !== 201) {
+    throw new Error("Unable to create posts");
+  }
+
+  // Parses the server’s JSON response and returns the parsed data.
+  const data = await response.json();
+  return data;
+}
