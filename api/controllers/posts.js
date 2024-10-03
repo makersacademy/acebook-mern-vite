47e8a001db16568f9461a9297eac609058b0449c
Chris <cajcrook@gmail.com>
2024-10-03 13:26:09 +0100

    v1 getPostByUser, refort required

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,11 +1,20 @@
 const Post = require("../models/post");
 const { generateToken } = require("../lib/token");
+const User = require("../models/user");
 
 async function getAllPosts(req, res) {
   const posts = await Post.find().populate('user', 'username');
   const token = generateToken(req.user_id);
   res.status(200).json({ posts: posts, token: token });
 }
+
+async function getPostByUser(req, res) {
+  const user = await User.findOne().populate('post', 'username', 'user_id')
+  const token = generateToken(req.user_id);
+  res.status(200).json({ user: user, token: token})
+  
+}
+
 
 async function createPost(req, res) {
   const post = new Post(req.body);
@@ -18,6 +27,7 @@ async function createPost(req, res) {
 const PostsController = {
   getAllPosts: getAllPosts,
   createPost: createPost,
+  getPostByUser: getPostByUser,
 };
 
 module.exports = PostsController;
