802400a75a33d0ea7758d21fa8324512205924fc
Dewi Afoko <dewi.afoko@gmail.com>
2024-10-04 20:22:26 +0100

    Patch route added to users/me to add/update profile picture

--- api/routes/users.js
+++ api/routes/users.js
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post("/", UsersController.create);
 router.get("/", UsersController.getAllUsers);
 router.get("/me", tokenChecker, UsersController.getCurrentUser); //tokenChecker to check if valid token
+router.patch("/me", tokenChecker, UsersController.updateImgURL); //patch route to update existing user's profile picture
 
 module.exports = router;
