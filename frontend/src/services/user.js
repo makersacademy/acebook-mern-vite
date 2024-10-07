17c326d1969ea394c786f1f1bcbd6429cfb1d598
Chris <cajcrook@gmail.com>
2024-10-07 16:28:04 +0100

    user Service avec test

---
+++ frontend/src/services/user.js
@@ -0,0 +1,26 @@
+// docs: https://vitejs.dev/guide/env-and-mode.html
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export async function getUserInfo(token, userID="") {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const newUrl = new URL(`${BACKEND_URL}/posts`);
+  if (userID) {
+    newUrl.searchParams.append("userID",`${userID}`)
+  }
+  
+  const response = await fetch(newUrl.toString(), requestOptions);
+
+  if (response.status !== 200) {
+    throw new Error("Unable to fetch posts");
+  }
+
+  const data = await response.json();
+  return data;
+}
+
