2872555906bbd447e31dd392837cb624a285b058
Robert McIsaac <robmcisaac@hotmail.co.uk>
2024-10-07 10:37:59 +0100

    changed models

--- api/models/post.js
+++ api/models/post.js
@@ -2,11 +2,27 @@ const mongoose = require("mongoose");
 
 // A Schema defines the "shape" of entries in a collection. This is similar to
 // defining the columns of an SQL Database.
+
+// const PostSchema = new mongoose.Schema({
+//   user: String, // added user as a string
+//   message: { type: String,},  
+//     }, 
+//     { timestamps: true } 
+// ); 
+
 const PostSchema = new mongoose.Schema({
-  user: String, // added user as a string
-  message: { type: String,},  
-    }, 
-    { timestamps: true } 
+  message: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    count: 0, liked_by: [] //counts number of likes and a list of users who have liked a post
+  },
+  author: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true }
 ); 
 
 
--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  username: { type: String, required: true },
-});
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true 
+  },
+  username: {
+    type: String,
+    required: true 
+  }
+},
+{timstamps: true}
+);
 
 const User = mongoose.model("User", UserSchema);
 
