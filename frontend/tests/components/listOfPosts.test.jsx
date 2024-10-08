ec4711090607afab8803e3266ce931af40fe9563
Marion <marionjung@myyahoo.com>
2024-10-08 12:54:46 +0100

    Adding tests for ProfilePage and listOfPosts

--- frontend/tests/components/listOfPosts.test.jsx
+++ frontend/tests/components/listOfPosts.test.jsx
@@ -1,8 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import ListOfPosts from "../../src/components/ListOfPosts";
 
-
-
 describe("ListOfPosts", () => {
   const mockPosts = [
     { _id: "1",
@@ -26,40 +24,40 @@ describe("ListOfPosts", () => {
         user: {
             username: "Alexia",
         },
-        dateCreated: "2024-09-07",
+        dateCreated: "2024-10-07T15:00:07.120Z",
         noOfLikes: "20",
     },
   ];
-  
-    test("renders multiple posts correctly - usernames", () => {
+
+  test("renders multiple posts correctly - usernames in chrological order", () => {
         render(<ListOfPosts posts={mockPosts} />);
         const post = screen.getAllByTestId("user-link");
         expect(post[0].textContent).toEqual("Alexia")
-        expect(post[1].textContent).toEqual("Marion")
-        expect(post[2].textContent).toEqual("Alexia")
+        expect(post[2].textContent).toEqual("Marion")
+        expect(post[1].textContent).toEqual("Alexia")
     });
 
-  test("renders multiple posts correctly - message", () => {
+  test("renders multiple posts correctly - message in chrological order", () => {
         render(<ListOfPosts posts={mockPosts} />);
         const post = screen.getAllByTestId("message");
         expect(post[0].textContent).toEqual("Test message")
-        expect(post[1].textContent).toEqual("Test message 2")
-        expect(post[2].textContent).toEqual("Test message 3")
+        expect(post[2].textContent).toEqual("Test message 2")
+        expect(post[1].textContent).toEqual("Test message 3")
     });
 
-    test("renders multiple posts correctly - date created", () => {
+    test("renders multiple posts correctly - date created in chrological order", () => {
         render(<ListOfPosts posts={mockPosts} />);
         const post = screen.getAllByTestId("dateCreated");
         expect(post[0].textContent).toEqual("1:00pm - 07.10.24")
-        expect(post[1].textContent).toEqual("9:31am - 08.10.24")
-        expect(post[2].textContent).toEqual("12:00am - 07.09.24")
+        expect(post[1].textContent).toEqual("3:00pm - 07.10.24")
+        expect(post[2].textContent).toEqual("9:31am - 08.10.24")
     });
 
-    test("renders multiple posts correctly - likes", () => {
+    test("renders multiple posts correctly - likes in chrological order", () => {
         render(<ListOfPosts posts={mockPosts} />);
         const post = screen.getAllByTestId("numberOfLikes");
         expect(post[0].textContent).toEqual("15 Likes")
-        expect(post[1].textContent).toEqual("16 Likes")
-        expect(post[2].textContent).toEqual("20 Likes")
+        expect(post[2].textContent).toEqual("16 Likes")
+        expect(post[1].textContent).toEqual("20 Likes")
     });
 });
