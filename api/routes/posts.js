24facef8f9097a79c1f4255f620f2dffc3bb6932
MichaelV00 <mwverdon2000@gmail.com>
2024-10-08 11:08:07 +0100

    sick

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -6,6 +6,7 @@ const AuthenticationController = require("../controllers/authentication");
 
 router.get("/", PostsController.getAllPosts); // a http get request will fetch the posts
 router.post("/", PostsController.createPost); // a http post request will create the post as defined in api/controllers/posts.js
-router.delete("/:id", PostsController.deletePost); 
+router.delete("/:id", PostsController.deletePost);
+// Change to posts/:id ????
 
 module.exports = router;
