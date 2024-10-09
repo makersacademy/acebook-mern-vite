e80590d06d879b1158e8cc993ad1b0c899d4cf8a
Alexia-May <alexia.maynart@gmail.com>
2024-10-09 10:24:16 +0100

    adding user.js file

--- api/models/user.js
+++ api/models/user.js
@@ -3,14 +3,13 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  username: String,
-  firstName: String,
-  lastName: String,
-  gender: String,
-  birthday: Date,
+  username: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  gender: { type: String, required: true },
+  birthday: { type: Date, required: true },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   likedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
-
 });
 
 const User = mongoose.model("User", UserSchema);
