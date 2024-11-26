dc0cdc60c3667d506eb53f2979c82ab58a53b8a2
Jamie <jamieplumy@gmail.com>
2024-11-26 11:21:50 +0000

    changed file names

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { HomePage } from "./pages/Home/HomePage";
 import { LoginPage } from "./pages/Login/LoginPage";
 import { SignupPage } from "./pages/Signup/SignupPage";
 import { FeedPage } from "./pages/Feed/FeedPage";
-import { Profile } from "./pages/Profile";
+import { Profile } from "./pages/Profile/ProfilePage";
 
 // docs: https://reactrouter.com/en/main/start/overview
 const router = createBrowserRouter([
