4fa101edd91fb5818162a7df4da617582d39b7c7
Joshua Blackmore <joshuablackmore@gmail.com>
2024-11-26 16:45:02 +0000

    sorted by most recent post in serverside and displayed username and date

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,5 +1,16 @@
 function Post(props) {
-  return <article key={props.post._id}>{props.post.message}</article>;
+  return (
+    <div>
+      <h2>{props.user}</h2>
+      <h3>
+        {props.timestamp
+          .replace("Z", "")
+          .replace(":00.000", "")
+          .replace("T", " ")}
+      </h3>
+      <article key={props._id}>{props.message}</article>
+    </div>
+  );
 }
 
 export default Post;
