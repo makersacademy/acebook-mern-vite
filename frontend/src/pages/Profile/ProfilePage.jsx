f75d82b47e5eb9dab5900309649844ce63f9e059
Patrick Shannon <shannon.patrick06@gmail.com>
2024-11-26 15:14:13 +0000

    Added navbar, updated logoutbutton component.

--- frontend/src/pages/Profile/ProfilePage.jsx
+++ frontend/src/pages/Profile/ProfilePage.jsx
@@ -1,7 +1,14 @@
+import { NavBar } from "../../components/NavBar";
+
 export function Profile() {
   return (
+    <>
+    <NavBar />
+    <body>
     <div className="Profile">
       <h1>My profile page</h1>
     </div>
+    </body>
+    </>
   );
 }
