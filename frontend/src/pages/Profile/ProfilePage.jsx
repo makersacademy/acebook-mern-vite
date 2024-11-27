f10bc31a4e7d803b71429509e869e7fd81139bee
Abdirahman <mraaden10@gmail.com>
2024-11-27 12:12:36 +0000

    profile :)

---
+++ frontend/src/pages/Profile/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+
+export function ProfilePage() {
+    return (
+      <div className="profile">
+        <h1>Welcome to your COFFEE COUNTER!</h1>
+      </div>
+    );
+  }
+  
