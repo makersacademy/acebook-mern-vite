bda2589759676f03411dac78281093e88e10550c
Jamie <jamieplumy@gmail.com>
2024-11-28 16:57:22 +0000

    GET /users method for getting own user details, all tests pass, we are doing this properly

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -23,3 +23,39 @@ const PostsController = {
 };
 
 module.exports = PostsController;
+
+
+
+// const mongoose = require("mongoose");
+// const Post = require("../models/post");
+// const { generateToken, decodeToken } = require("../lib/token");
+
+// async function getAllPosts(req, res) {
+//   const posts = await Post.find({ userId: req.user_id });
+//   const token = generateToken(req.user_id);
+//   // console.log(req.user_id)
+//   res.status(200).json({ posts: posts, token: token });
+// }
+
+// async function createPost(req, res) {
+//   // console.log(req);
+//   // console.log(decodeToken(req))
+//   const newPostData = {
+//     message: req.body.message,
+//     userId: new mongoose.Types.ObjectId(req.user_id)
+//   }
+//   const post = new Post(newPostData);
+//   post.save();
+
+//   const newToken = generateToken(req.user_id);
+//   console.log(newToken);
+//   console.log(decodeToken(newToken));
+//   res.status(201).json({ message: "Post created", token: newToken });
+// }
+
+// const PostsController = {
+//   getAllPosts: getAllPosts,
+//   createPost: createPost,
+// };
+
+// module.exports = PostsController;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,9 @@
 const express = require("express");
+const router = express.Router();
 
 const UsersController = require("../controllers/users");
 
-const router = express.Router();
-
+router.get("/", UsersController.getUserProfile);
 router.post("/", UsersController.create);
-
-// router.get('/profile', UsersController.getUserProfile);
 
 module.exports = router;
