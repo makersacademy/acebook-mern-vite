904c97ff97923ee723a2684cbe89a15f62f7cd36
Jamie King <jamiedanielking@gmail.com>
2024-11-28 11:59:52 +0000

    Added bean button, counter and action

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -4,8 +4,14 @@ const { generateToken } = require("../lib/token");
 async function getAllPosts(req, res) {
   const posts = await Post.find();
   posts.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+
+  const likedPosts = posts.map((post) => {
+    post._doc.hasLiked = (post.beans.includes(req.username))
+    return post
+  })
+
   const token = generateToken(req.user_id, req.username);
-  res.status(200).json({ posts: posts, token: token });
+  res.status(200).json({ posts: likedPosts, token: token });
 }
 
 async function createPost(req, res) {
@@ -50,6 +56,27 @@ async function updatePost(req, res) {
   res.status(200).json({ message: "Post updated", token: newToken });
 }
 
+async function likePost(req, res) {
+  const postId = req.params.post_id
+  const post = await Post.findOne({_id: postId})
+  const hasLiked = post.beans.includes(req.username)
+  
+  if (hasLiked) {
+    await Post.findOneAndUpdate(
+      {_id: postId},
+      {$pull: { beans: req.username }}
+    )
+  } else {
+    await Post.findOneAndUpdate(
+      {_id: postId},
+      {$push: { beans: req.username }}
+    )
+  }
+
+  const newToken = generateToken(req.user_id, req.username);
+  res.status(200).json({ message: "Likes changed", token: newToken })
+}
+
 const PostsController = {
   getAllPosts: getAllPosts,
   createPost: createPost,
@@ -57,6 +84,7 @@ const PostsController = {
   updatePost: updatePost,
   getUserPosts: getUserPosts,
   getYourPosts: getYourPosts,
+  likePost: likePost,
 };
 
 module.exports = PostsController;
