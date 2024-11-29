a9e9cd82d4f7a650a4dee8de942f98a44d8daee6
Frankie <frankiepike@live.co.uk>
2024-11-29 10:00:22 +0000

    frontend photo upload additions

--- frontend/src/pages/Profile/ProfilePage.jsx
+++ frontend/src/pages/Profile/ProfilePage.jsx
@@ -1,14 +1,16 @@
 import { NavBar } from "../../components/NavBar";
+import { PhotoUpload } from "../../components/PhotoUpload";
 
 export function Profile() {
   return (
     <>
     <NavBar />
-    <body>
+    {/* <body> */}
     <div className="Profile">
       <h1>My profile page</h1>
+      <PhotoUpload />
     </div>
-    </body>
+    {/* </body> */}
     </>
   );
 }
